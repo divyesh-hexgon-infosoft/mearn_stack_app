25d4be4a985125db9828c930c31332a3b9d36926
divyesh-hexgon-infosoft <167194176+divyesh-hexgon-infosoft@users.noreply.github.com>
2024-08-24 14:47:24 +0530

    Update mern_stack_app.sql

--- mern_stack_app.sql
+++ mern_stack_app.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: mearn_stack_app
+-- Host: localhost    Database: mern_stack_app
 -- ------------------------------------------------------
 -- Server version	8.0.34
 
